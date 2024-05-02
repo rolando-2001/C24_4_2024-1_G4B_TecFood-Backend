3a02041aee76cbf567b509d0885eabe89d4e6c24
rolando-2001 <rolando.casapaico@tecsup.edu.pe>
2024-05-02 11:22:01 -0500

    first commit

---
+++ spring-boot-backend/springboot-api-rest/src/main/java/com/backend/app/SpringbootApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.backend.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApiRestApplication.class, args);
+	}
+
+}
