4a18a92f5c88e9c8357a7d0bc7a2dabd1a46f4f6
rolando-2001 <rolando.casapaico@tecsup.edu.pe>
2024-05-02 14:21:00 -0500

    first commit

--- spring-boot-backend/TecFood/src/main/java/com/backend/app/TecFoodApplication.java
+++ spring-boot-backend/TecFood/src/main/java/com/backend/app/TecFoodApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootApiRestApplication {
+public class TecFoodApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApiRestApplication.class, args);
+		SpringApplication.run(TecFoodApplication.class, args);
 	}
 
 }
